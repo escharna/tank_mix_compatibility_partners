03734e232d211635a1cd2894439cd12c8047e706
Eric Scharnak <ericscharnak@erics-mbp.attlocal.net>
2018-09-29 16:39:17 -0400

    Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :static_pages
 
+  resources :users
+
+  authenticated :user do
+    root 'users#index', as: :authenticated_root
+  end
+
+  resources :static_pages
   root to: 'static_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
