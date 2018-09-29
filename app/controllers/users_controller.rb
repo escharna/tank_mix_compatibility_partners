08a3b7d2daea944d43ac75c76c2305d4af464c53
Eric Scharnak <ericscharnak@erics-mbp.attlocal.net>
2018-09-29 18:15:51 -0400

    Add additional user attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :get_user, only: [:edit, :update]
 
   def index
     @users = User.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_attributes)
+      redirect_to users_path
+    else
+      raise 'check'
+    end
+  end
+
+  private
+
+  def user_attributes
+    params.require(:user).permit(:id, :first_name, :last_name, :job_title)
+  end
+
+  def get_user
+    @user = User.find(params[:id])
   end
 end
